cd2454d0ec91a56fa4bec66915e13b4e52f11c81
zach1492 <zachmbrough@gmail.com>
2026-05-29 11:32:08 +1200

    Started on Worker

--- server/FileTransferWorker.cs
+++ server/FileTransferWorker.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Net.IO
 
 public class FileTransferWorker
 {
@@ -22,5 +23,47 @@ public class FileTransferWorker
     // (Job: Filename, ClientEndpoint, TransferSocket).
     public static void Run(object jobObject)
     {
+        string message = $"ERR {filename} NOT_FOUND";
+
+        if(!file.Exists(filename)){
+            SendControlReply(ClientEndpoint, message);
+            socket.Shutdown(SocketShutdown.Send)
+            transferSocket.close();
+            
+        }
+        else{
+            p = PublicPort(job.TransferSocket)
+            message = $"OK {filename} SIZE {new FileInfo(path).Length} PORT {p/*IPEndPoint*/} ";
+
+            byte[] bytes = Encoding.ASCII.GetBytes(message);
+
+            job.transferSocket.send(bytes, bytes.Length, 0, SocketFlags.None);
+
+            while(true){
+                byte[] bytes = transferSocket.Receive(ref groupEP);
+
+                string message = Encoding.ASCII.GetString(bytes);
+
+                int startWord = message.IndexOf("FILE ") + 5;
+                int endWord = message.IndexOf(" GET");
+
+                string fileName = message.Substring(startWord, endWord - startWord);
+
+                startWord = message.IndexOf("START ") + 6;
+                endWord = message.IndexOf(" END");
+
+                int startIndex = int.Parse(message.Substring(startWord, endWord - startWord));
+
+                startWord = message.IndexOf(" END") + 4;
+
+                int endIndex = int.Parse(message.Substring(startWord, message.Length - startWord));
+
+                string file = File.ReadAllLines(fileName);
+
+                int length = endIndex - startIndex 
+
+                file = file.Substring(startIndex, length);
+            }
+        }
     }
 }
